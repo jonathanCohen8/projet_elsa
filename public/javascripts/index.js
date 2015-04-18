a02f708a7cccf92315f056d214971e105ca80fd7
avalown <sousoues@gmail.com>
2015-04-18 15:38:49 +0200

    Inventaire quasi fonctionnel

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,7 +5,6 @@ var joueur1 = {
     acuity: 3,
     move:2
 };
-var table = [[25,39,39,0],[25,39,39,0],[0,0,0,0],[0,0,0,0]];
 
 
 //Fonction calcule dégrader des statistiques
@@ -29,15 +28,39 @@ function color_timer(){
 }
 
 //Fonction inventaire
-	function inventory(table) {
+	function inventory(user) {
 		//Définition variable de départ
 		var box1 = '#object1',
 			box2 = '#object2',
+			inv = '#table1',
+			inv_check = [],
+			table = user[0].bag,
 			cell_size = 43;
 
 		// Positionnement initial des objets
-		$(box1).css('left', cell_size + 4);
-		
+		//On recupere les coordonnées de la grille
+		invPos= $(inv).position();
+		var bag_size = table.length
+		//On va lire le tableau et déposer les objet
+		for (var i=0 ; i<bag_size ; i++) {
+			for (var j=0 ;j<bag_size ; j++) { 
+				if (table[i][j] !== 0)
+				{
+					var y = 0;
+					for (var k=0 ; k< inv_check.length ; k++) {
+						if (inv_check[k] === table[i][j]){
+							y = 1;
+						} 
+					}
+					if (y === 0){
+						inv_check.push(table[i][j]);
+						$('img[data-objectId='+table[i][j]+']').css('left',invPos.left + 4 + (cell_size * j));
+						$('img[data-objectId='+table[i][j]+']').css('top',invPos.top + 4 + (cell_size * i));
+					}
+				}
+			}
+		}
+
 		//On rend l'objet draggable
 		$(box1).draggable({ grid: [ cell_size, cell_size ] },
 		{
@@ -55,25 +78,25 @@ function color_timer(){
 		$("#dragMe").draggable();
 				
 		//On execute la fonction pour chaque objet de l'inventaire
-		collide(box1,39,table);
-		collide(box2,25,table);
+		collide(box1,1,table);
+		collide(box2,2,table);
 	}
 
 	//Gestion collision des objets
 	function collide(box, id, table){
-		var drop = $('td'),
-		size = 4;
+		var drop = $('td');
 		$(box).on('mousedown', function(e) {
-			for (var i=0 ; i<size ; i++) {
-				for (var j=0 ; j<size ; j++) { 
-					if(table[i][j] == id)
+			//On supprime du tableau table les case contenant le meme id que l'objet clicker
+			for (var i=0 ; i<table.length ; i++) {
+				for (var j=0 ; j<table.length ; j++) { 
+					if(table[i][j] === id)
 						table[i][j] = 0;
 					}
 			}
 			$(document).on('mousemove', function(e) {
 				//Gestion de l'identifiant dans la table HTML
 				$('#table1').find("tr").find("td").each(function(){
-					if ($(this).hasClass('under') == true){
+					if ($(this).hasClass('under') === true){
 						$(this).html(id);
 					}
 					else{
@@ -89,7 +112,7 @@ function color_timer(){
 
 			}).on('mouseup', function() {
 				$(this).off('mousemove');
-				//Creation du tableau de valeur représentant la "carte" 
+				//Creation du tableau de valeur représentant la "carte"
 				var tbl = $('#table1 tr').get().map(function(row) {
 					return $(row).find('td').get().map(function(cell) {
 						return parseInt($(cell).html());
@@ -97,10 +120,10 @@ function color_timer(){
 				});
 
 				//On lit le tableau (tbl) et on le compare avec celui en mémoire (table)
-				for (var i=0 ; i<size ; i++) {
-					for (var j=0 ; j<size ; j++) { 
+				for (var i=0 ; i<table.length ; i++) {
+					for (var j=0 ; j<table.length ; j++) { 
 						if (tbl[i][j] !== table[i][j]){
-							if (table[i][j] == 0)
+							if (table[i][j] === 0)
 								table[i][j] = tbl[i][j];
 						}
 					}
@@ -126,7 +149,7 @@ $(document).ready(function() {
 		$('#equipment').animate({bottom:0},400);
 	    $('#timer').animate({bottom:-200},400);
 	    //On appel la fonction inventaire
-		inventory(table);
+		inventory(user);
 	});
 
 	//Au click sur la croix de l'inventaire
